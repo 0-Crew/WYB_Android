73c8ffcf2375145091c562988b0c9065e5c1f18b
heewon <ymcho24@naver.com>
2022-04-11 15:37:50 +0900

    [feat] WYBPopupWindowItemAdapter 의 매개변수로 viewType 을 받도록 수정

--- app/src/main/java/com/wyb/wyb_android/widget/WYBPopupWindowItemAdapter.kt
+++ app/src/main/java/com/wyb/wyb_android/widget/WYBPopupWindowItemAdapter.kt
@@ -6,10 +6,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.databinding.ItemWybPopupWindowBinding
+import com.wyb.wyb_android.databinding.ItemWybPopupWindowSmallBinding
 import kotlinx.android.synthetic.main.item_wyb_popup_window.view.*
 
-class WYBPopupWindowItemAdapter :
-    RecyclerView.Adapter<WYBPopupWindowItemAdapter.WYBPopupWindowItemViewHolder>() {
+class WYBPopupWindowItemAdapter(private val viewType: Int) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var selectedPos = RecyclerView.NO_POSITION
     private var data = listOf(
         R.string.popup_menu_plastic_dining_utensil,
@@ -28,31 +29,51 @@ class WYBPopupWindowItemAdapter :
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): WYBPopupWindowItemViewHolder {
-        val binding =
-            ItemWybPopupWindowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return WYBPopupWindowItemViewHolder(binding, parent.context)
+    ): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_POPUP_DEFAULT -> {
+                val binding =
+                    ItemWybPopupWindowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                WYBPopupWindowItemViewHolder(binding, parent.context)
+            }
+            else -> {
+                val binding =
+                    ItemWybPopupWindowSmallBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                WYBPopupWindowItemSmallViewHolder(binding, parent.context)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: WYBPopupWindowItemViewHolder, position: Int) {
-        holder.bind(data[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is WYBPopupWindowItemViewHolder -> {
+                holder.bind(data[position])
 
-        if (selectedPos == RecyclerView.NO_POSITION && position == 0) {
-            selectedPos = 0
-            holder.itemView.isSelected = true
-            holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Bold_14)
-        } else {
-            if (selectedPos == position) {
-                holder.itemView.isSelected = true
-                holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Bold_14)
-            } else {
-                holder.itemView.isSelected = false
-                holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Medium_14)
+                if (selectedPos == RecyclerView.NO_POSITION && position == 0) {
+                    selectedPos = 0
+                    holder.itemView.isSelected = true
+                    holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Bold_14)
+                } else {
+                    if (selectedPos == position) {
+                        holder.itemView.isSelected = true
+                        holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Bold_14)
+                    } else {
+                        holder.itemView.isSelected = false
+                        holder.itemView.tvItem.setTextAppearance(R.style.TextAppearance_WYBComponents_PopupWindow_Medium_14)
+                    }
+                }
+            }
+            is WYBPopupWindowItemSmallViewHolder -> {
+                holder.bind(data[position])
             }
         }
     }
 
     override fun getItemCount(): Int = data.size
+
+    override fun getItemViewType(position: Int): Int {
+        return viewType
+    }
 
     inner class WYBPopupWindowItemViewHolder(
         private val binding: ItemWybPopupWindowBinding,
@@ -67,5 +88,19 @@ class WYBPopupWindowItemAdapter :
                 notifyItemChanged(selectedPos)
             }
         }
+    }
+
+    inner class WYBPopupWindowItemSmallViewHolder(
+        private val binding: ItemWybPopupWindowSmallBinding,
+        private val context: Context
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(stringRes: Int) {
+            binding.tvItemSmall.text = context.getString(stringRes)
+        }
+    }
+
+    companion object {
+        const val TYPE_POPUP_DEFAULT = 0
+        const val TYPE_POPUP_SMALL = 1
     }
 }
