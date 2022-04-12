46de1be759667cf8da90ba81c532c21eddf2587c
heewon <ymcho24@naver.com>
2022-04-13 01:22:11 +0900

    [feat] 챌린지 오픈 화면에 ChallengeOpenViewModel 연결

--- app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenComfortFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenComfortFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.navGraphViewModels
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.base.BindingFragment
 import com.wyb.wyb_android.databinding.FragmentChallengeOpenComfortBinding
 
 class ChallengeOpenComfortFragment :
     BindingFragment<FragmentChallengeOpenComfortBinding>(R.layout.fragment_challenge_open_comfort) {
+    private val viewModel: ChallengeOpenViewModel by navGraphViewModels(R.id.challenge_open_nav_graph)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        super.onCreateView(inflater, container, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
         return binding.root
     }
 }
