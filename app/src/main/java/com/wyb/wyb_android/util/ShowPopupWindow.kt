6a4ae9c75d9ac4e1d07ce99d99a6280700644532
gaeun23 <0423g@naver.com>
2022-04-17 22:53:15 +0900

    [feat] type 에 따라 extension 분리

--- app/src/main/java/com/wyb/wyb_android/util/ShowPopupWindow.kt
+++ app/src/main/java/com/wyb/wyb_android/util/ShowPopupWindow.kt
@@ -6,22 +6,20 @@ import android.view.View
 import android.widget.PopupWindow
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.wyb.wyb_android.R
 import com.wyb.wyb_android.databinding.ViewWybPopupWindowBinding
 import com.wyb.wyb_android.widget.WYBPopupWindowItemAdapter
 import com.wyb.wyb_android.widget.WYBPopupWindowItemAdapter.Companion.TYPE_POPUP_DEFAULT
+import com.wyb.wyb_android.widget.WYBPopupWindowItemAdapter.Companion.TYPE_POPUP_SMALL
 
-fun View.showPopupWindow(
-    backgroundRes: Int,
-    context: Context,
-    type: Int
+fun View.showPopupWindowDefault(
+    context: Context
 ) {
     val popupView = ViewWybPopupWindowBinding.inflate(LayoutInflater.from(context))
-    initPopupView(popupView, type, context)
+    initPopupView(popupView, TYPE_POPUP_DEFAULT, context)
 
-    val popupHeight = when (type) {
-        TYPE_POPUP_DEFAULT -> 158
-        else -> 134
-    }
+    val popupHeight: Int = resources.getInteger(R.integer.wyb_popup_window_height_default)
+    val popupMargin: Int = resources.getInteger(R.integer.wyb_popup_window_margin_default)
 
     val popup = PopupWindow(
         popupView.root,
@@ -33,11 +31,38 @@ fun View.showPopupWindow(
     popup.setBackgroundDrawable(
         ResourcesCompat.getDrawable(
             context.resources,
+            R.drawable.shape_orange_stroke,
+            null
+        )
+    )
+    popup.showAsDropDown(this, 0, convertDpToPx(context, popupMargin))
+}
+
+fun View.showPopupWindowSmall(
+    backgroundRes: Int,
+    context: Context,
+    margin: Int
+) {
+    val popupView = ViewWybPopupWindowBinding.inflate(LayoutInflater.from(context))
+    initPopupView(popupView, TYPE_POPUP_SMALL, context)
+
+    val popupHeight: Int = resources.getInteger(R.integer.wyb_popup_window_height_small)
+
+    val popup = PopupWindow(
+        popupView.root,
+        this.width,
+        convertDpToPx(context, popupHeight),
+        true
+    )
+
+    popup.setBackgroundDrawable(
+        ResourcesCompat.getDrawable(
+            context.resources,
             backgroundRes,
             null
         )
     )
-    popup.showAsDropDown(this, 0, convertDpToPx(context, 6))
+    popup.showAsDropDown(this, 0, convertDpToPx(context, margin))
 }
 
 private fun initPopupView(popupWindow: ViewWybPopupWindowBinding, type: Int, context: Context) {
