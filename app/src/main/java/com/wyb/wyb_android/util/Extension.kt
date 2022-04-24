3f661cc40e041472227fc39510c8d8e91c1a7e4e
gaeun23 <0423g@naver.com>
2022-04-25 02:45:45 +0900

    [chore] package refactoring

--- app/src/main/java/com/wyb/wyb_android/util/Extension.kt
+++
@@ -1,9 +0,0 @@
-package com.wyb.wyb_android.util
-
-import android.content.Context
-import kotlin.math.roundToInt
-
-fun convertDpToPx(context: Context, dp: Int): Int {
-    val density = context.resources.displayMetrics.density
-    return (dp.toFloat() * density).roundToInt()
-}
