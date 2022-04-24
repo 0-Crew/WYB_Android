b847682623cdf7c162f3fb797e20b8ac61e495fa
heewon <ymcho24@naver.com>
2022-04-25 00:36:27 +0900

    [fix] 챌린지 오픈 관련 fragment 가 ViewModelFragment 를 상속받도록 수정

--- app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenDiscomfortFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenDiscomfortFragment.kt
@@ -6,19 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.navGraphViewModels
 import com.wyb.wyb_android.R
-import com.wyb.wyb_android.base.BindingFragment
+import com.wyb.wyb_android.base.ViewModelFragment
 import com.wyb.wyb_android.databinding.FragmentChallengeOpenDiscomfortBinding
 
 class ChallengeOpenDiscomfortFragment :
-    BindingFragment<FragmentChallengeOpenDiscomfortBinding>(R.layout.fragment_challenge_open_discomfort) {
-    private val viewModel: ChallengeOpenViewModel by navGraphViewModels(R.id.challenge_open_nav_graph)
+    ViewModelFragment<FragmentChallengeOpenDiscomfortBinding, ChallengeOpenViewModel>(R.layout.fragment_challenge_open_discomfort) {
+    override val viewModel: ChallengeOpenViewModel by navGraphViewModels(R.id.challenge_open_nav_graph)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = viewModel
-
+        super.onCreateView(inflater, container, savedInstanceState)
         return binding.root
     }
 }
