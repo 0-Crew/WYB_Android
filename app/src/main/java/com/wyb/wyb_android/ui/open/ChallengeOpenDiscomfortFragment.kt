03c6bc4e350f4384ea72f32f43d02c2ebdb3b9f1
heewon <ymcho24@naver.com>
2022-04-26 20:46:41 +0900

    [layout] ChallengeOpenDiscomfortFragment 레이아웃 구현

--- app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenDiscomfortFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/open/ChallengeOpenDiscomfortFragment.kt
@@ -8,6 +8,7 @@ import androidx.navigation.navGraphViewModels
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.base.ViewModelFragment
 import com.wyb.wyb_android.databinding.FragmentChallengeOpenDiscomfortBinding
+import com.wyb.wyb_android.util.showPopupWindow
 
 class ChallengeOpenDiscomfortFragment :
     ViewModelFragment<FragmentChallengeOpenDiscomfortBinding, ChallengeOpenViewModel>(R.layout.fragment_challenge_open_discomfort) {
@@ -17,6 +18,19 @@ class ChallengeOpenDiscomfortFragment :
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+
+        initNavBar()
+        binding.etDiscomfort.post { binding.etDiscomfort.showPopupWindow(requireContext()) }
+
         return binding.root
+    }
+
+    private fun initNavBar() {
+        binding.includeNavBar.apply {
+            btnNav.setImageResource(R.drawable.ic_arrow_left)
+            tvNavTitle.text = getString(R.string.challenge_open_discomfort_nav_title)
+            ivBottle.setImageResource(R.drawable.ic_nav_bottle_washing)
+            progressBar.progress = 2
+        }
     }
 }
