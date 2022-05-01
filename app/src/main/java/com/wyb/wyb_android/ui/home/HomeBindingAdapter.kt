3b338a149d34fbb6caedfd316b2863839c90552b
gaeun23 <0423g@naver.com>
2022-05-01 17:43:49 +0900

    [layout] 주스 양에 따라 bottle 이미지 변경

---
+++ app/src/main/java/com/wyb/wyb_android/ui/home/HomeBindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.wyb.wyb_android.ui.home
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.wyb.wyb_android.R
+
+@BindingAdapter("setBottleSrc")
+fun bindBottleImg(imageView: ImageView, levelOfJuice: Int) {
+    when (levelOfJuice) {
+        7 -> imageView.setImageResource(R.drawable.ic_bottle_main_0)
+        6 -> imageView.setImageResource(R.drawable.ic_bottle_main_1)
+        5 -> imageView.setImageResource(R.drawable.ic_bottle_main_2)
+        4 -> imageView.setImageResource(R.drawable.ic_bottle_main_3)
+        3 -> imageView.setImageResource(R.drawable.ic_bottle_main_4)
+        2 -> imageView.setImageResource(R.drawable.ic_bottle_main_5)
+        1 -> imageView.setImageResource(R.drawable.ic_bottle_main_6)
+        0 -> imageView.setImageResource(R.drawable.ic_bottle_main_7)
+        else -> imageView.setImageResource(R.drawable.ic_bottle_main_0)
+    }
+}
