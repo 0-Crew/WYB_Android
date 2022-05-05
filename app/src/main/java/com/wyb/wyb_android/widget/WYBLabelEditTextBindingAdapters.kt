c079087866672db259f75283175b33d35936887f
heewon <ymcho24@naver.com>
2022-05-05 21:03:57 +0900

    [fix] CustomView BindingAdapter 함수 선언 방식 변경

--- app/src/main/java/com/wyb/wyb_android/widget/WYBLabelEditTextBindingAdapters.kt
+++ app/src/main/java/com/wyb/wyb_android/widget/WYBLabelEditTextBindingAdapters.kt
@@ -8,46 +8,40 @@ import androidx.databinding.InverseBindingListener
 import com.wyb.wyb_android.R
 import kotlinx.android.synthetic.main.view_wyb_label_edit_text.view.*
 
-object WYBLabelEditTextBindingAdapters {
-    @BindingAdapter("inputText")
-    @JvmStatic
-    fun setInputText(view: WYBLabelEditText, inputText: String?) {
-        if (inputText.isNullOrBlank()) {
-            view.etInput.text.clear()
-        } else if (inputText != view.etInput.text.toString()) {
-            view.etInput.setText(inputText)
+@BindingAdapter("inputText")
+fun setInputText(view: WYBLabelEditText, inputText: String?) {
+    if (inputText.isNullOrBlank()) {
+        view.etInput.text.clear()
+    } else if (inputText != view.etInput.text.toString()) {
+        view.etInput.setText(inputText)
+    }
+}
+
+@BindingAdapter("inputTextAttrChanged")
+fun setWYBLabelEditTextInverseBindingListener(
+    view: WYBLabelEditText,
+    listener: InverseBindingListener?
+) {
+    view.etInput.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable?) {
+            listener?.onChange()
         }
-    }
+    })
+}
 
-    @BindingAdapter("inputTextAttrChanged")
-    @JvmStatic
-    fun setWYBLabelEditTextInverseBindingListener(
-        view: WYBLabelEditText,
-        listener: InverseBindingListener?
-    ) {
-        view.etInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                listener?.onChange()
-            }
-        })
-    }
+@InverseBindingAdapter(attribute = "inputText", event = "inputTextAttrChanged")
+fun getInputText(view: WYBLabelEditText): String {
+    return view.etInput.text.toString()
+}
 
-    @InverseBindingAdapter(attribute = "inputText", event = "inputTextAttrChanged")
-    @JvmStatic
-    fun getInputText(view: WYBLabelEditText): String {
-        return view.etInput.text.toString()
-    }
-
-    @BindingAdapter("setBackgroundStroke")
-    @JvmStatic
-    fun setBackgroundStroke(view: WYBLabelEditText, color: Int) {
-        val gray = view.context.resources.getInteger(R.integer.wyb_label_edit_text_bg_stroke_gray)
-        val orange = view.context.resources.getInteger(R.integer.wyb_label_edit_text_bg_stroke_orange)
-        when (color) {
-            gray -> view.setBackgroundStroke(gray)
-            orange -> view.setBackgroundStroke(orange)
-        }
+@BindingAdapter("setBackgroundStroke")
+fun setBackgroundStroke(view: WYBLabelEditText, color: Int) {
+    val gray = view.context.resources.getInteger(R.integer.wyb_label_edit_text_bg_stroke_gray)
+    val orange = view.context.resources.getInteger(R.integer.wyb_label_edit_text_bg_stroke_orange)
+    when (color) {
+        gray -> view.setBackgroundStroke(gray)
+        orange -> view.setBackgroundStroke(orange)
     }
 }
