3eeb6548330745ebfc0541407f808c0cb947476e
gaeun23 <0423g@naver.com>
2022-06-01 18:04:12 +0900

    [feat] popupWindow 연결 및 레이아웃 초기 설정

--- app/src/main/java/com/wyb/wyb_android/ui/home/HomeChallengeAdapter.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/home/HomeChallengeAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
 import androidx.core.content.res.ResourcesCompat.getColorStateList
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -11,18 +12,43 @@ import androidx.recyclerview.widget.RecyclerView
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.data.model.Challenge
 import com.wyb.wyb_android.databinding.ItemHomeChallengeBinding
+import com.wyb.wyb_android.extension.showPopupWindow
+import com.wyb.wyb_android.widget.adapter.WYBPopupWindowItemAdapter
+import com.wyb.wyb_android.widget.adapter.WYBPopupWindowItemAdapter.Companion.TYPE_POPUP_SMALL
 
 class HomeChallengeAdapter(
     private val viewModel: HomeViewModel,
     private val context: Context
 ) : ListAdapter<Challenge, HomeChallengeAdapter.ChallengeViewHolder>(ChallengeDiffUtil()) {
+    private lateinit var popupWindow: PopupWindow
 
     inner class ChallengeViewHolder(
         private val binding: ItemHomeChallengeBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(challengeData: Challenge) {
             binding.data = challengeData
+            initView(challengeData)
             setBtnWaterCheckedListener(challengeData)
+        }
+
+        private fun initView(data: Challenge) {
+            when (data.isToday) {
+                true -> {
+                    initPopupWindow(R.drawable.shape_orange_stroke, data.day)
+                    binding.tvDiscomfort.setTextAppearance(R.style.TextAppearance_WYBComponents_Bold_14)
+                    setTodayCheckedView(binding.cbWaterDrop.isChecked)
+                }
+                else -> {
+                    initPopupWindow(R.drawable.shape_gray4_stroke, data.day)
+                    initTvDiscomfortTextColor(
+                        if (data.isFuture) {
+                            R.color.gray_2
+                        } else {
+                            R.color.gray_4
+                        }
+                    )
+                }
+            }
         }
 
         private fun setBtnWaterCheckedListener(data: Challenge) {
@@ -63,6 +89,38 @@ class HomeChallengeAdapter(
         private fun initTvDiscomfortTextColor(resourceId: Int) {
             binding.tvDiscomfort.apply {
                 setTextColor(resources.getColor(resourceId, null))
+            }
+        }
+
+        private fun initPopupWindow(
+            drawableRes: Int,
+            day: Int,
+        ) {
+            val popupWindowAdapter = WYBPopupWindowItemAdapter(context, TYPE_POPUP_SMALL)
+
+            binding.cbChallengeEdit.setOnCheckedChangeListener { view, isChecked ->
+                val resource = view.resources
+                if (isChecked) {
+                    popupWindow = binding.layoutChallengeToday.showPopupWindow(
+                        adapter = popupWindowAdapter,
+                        backgroundRes = drawableRes,
+                        context = context,
+                        margin = if (day > 5) {
+                            resource.getInteger(R.integer.wyb_popup_window_margin_small_up)
+                        } else {
+                            resource.getInteger(R.integer.wyb_popup_window_margin_small_down)
+                        }
+                    )
+                    binding.layoutChallengeToday.setBackgroundResource(drawableRes)
+
+                    popupWindow.setOnDismissListener {
+                        view.isChecked = false
+                    }
+                } else {
+                    binding.layoutChallengeToday.setBackgroundColor(
+                        resource.getColor(android.R.color.transparent, null)
+                    )
+                }
             }
         }
     }
