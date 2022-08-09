981331694ca817fafd4fdb7dac8ecaf09b9eeb71
heewon <ymcho24@naver.com>
2022-08-09 20:39:20 +0900

    [layout] 달력에 TodayDecorator 및 DateDecorator 적용

--- app/src/main/java/com/wyb/wyb_android/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/calendar/CalendarFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.databinding.FragmentCalendarBinding
@@ -24,6 +25,8 @@ class CalendarFragment : BottomSheetDialogFragment() {
         binding.viewModel = viewModel
 
         initCalendarLayout()
+        addDecorators()
+        addListener()
 
         return binding.root
     }
@@ -34,6 +37,44 @@ class CalendarFragment : BottomSheetDialogFragment() {
             leftArrow.setTint(resources.getColor(R.color.light_gray_1, null))
             rightArrow.setTint(resources.getColor(R.color.light_gray_1, null))
             setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.calendar_weekday)))
+        }
+    }
+
+    private fun addDecorators() {
+        binding.calendar.apply {
+            addDecorator(DateDecorator(requireContext()))
+            addDecorator(
+                TodayDecorator(
+                    requireContext(),
+                    resources.getColor(R.color.gray_1, null),
+                    resources.getColor(R.color.white, null),
+                    false
+                )
+            )
+        }
+    }
+
+    private fun addListener() {
+        binding.calendar.setOnDateChangedListener { widget, date, selected ->
+            if (selected && date == CalendarDay.today()) {
+                widget.addDecorator(
+                    TodayDecorator(
+                        requireContext(),
+                        resources.getColor(R.color.dark_gray_2, null),
+                        resources.getColor(R.color.dark_gray_2, null),
+                        true
+                    )
+                )
+            } else {
+                widget.addDecorator(
+                    TodayDecorator(
+                        requireContext(),
+                        resources.getColor(R.color.gray_1, null),
+                        resources.getColor(R.color.white, null),
+                        true
+                    )
+                )
+            }
         }
     }
 }
