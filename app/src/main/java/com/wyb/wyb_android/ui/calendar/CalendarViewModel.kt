a4411e6269c0e203df3ce915283dfe7665829b38
heewon <ymcho24@naver.com>
2022-08-16 18:58:08 +0900

    [feat] 서버에서 보내주는 time format 을 LocalDate 타입으로 변환하는 함수 구현

--- app/src/main/java/com/wyb/wyb_android/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/calendar/CalendarViewModel.kt
@@ -1,6 +1,25 @@
 package com.wyb.wyb_android.ui.calendar
 
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class CalendarViewModel : ViewModel() {
+    private fun getLocalDate(isoDate: String): LocalDate? {
+        // TODO: 서버 연동 후 timeZone Korea 로 변경 및 date format 변경
+        val sdf = SimpleDateFormat(ISO_DATE_FORMAT, Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        calendar.time = sdf.parse(isoDate) ?: return null
+        calendar.timeZone = TimeZone.getTimeZone("Etc/UTC")
+        return LocalDate.of(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+    }
+
+    companion object {
+        private const val ISO_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+    }
 }
