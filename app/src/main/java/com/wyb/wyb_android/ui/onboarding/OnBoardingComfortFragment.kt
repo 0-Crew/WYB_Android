590b8369d48559611254435878e52e04338e515f
gaeun23 <0423g@naver.com>
2022-10-02 17:11:00 +0900

    [feat] 온보딩 navigation graph 생성 및 fragment 이동 clickListener 지정

--- app/src/main/java/com/wyb/wyb_android/ui/onboarding/OnBoardingComfortFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/onboarding/OnBoardingComfortFragment.kt
@@ -1,5 +1,8 @@
 package com.wyb.wyb_android.ui.onboarding
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.base.BindingFragment
 import com.wyb.wyb_android.databinding.FragmentOnboardingComfortBinding
@@ -7,4 +10,14 @@ import com.wyb.wyb_android.databinding.FragmentOnboardingComfortBinding
 class OnBoardingComfortFragment : BindingFragment<FragmentOnboardingComfortBinding>(
     R.layout.fragment_onboarding_comfort
 ) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.actionOnBoardingComfortFragmentToOnBoardingChallengeFragment)
+        }
+    }
 }
--- app/src/main/java/com/wyb/wyb_android/ui/onboarding/OnBoardingIntroFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/onboarding/OnBoardingIntroFragment.kt
@@ -1,5 +1,8 @@
 package com.wyb.wyb_android.ui.onboarding
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.base.BindingFragment
 import com.wyb.wyb_android.databinding.FragmentOnboardingIntroBinding
@@ -7,4 +10,14 @@ import com.wyb.wyb_android.databinding.FragmentOnboardingIntroBinding
 class OnBoardingIntroFragment : BindingFragment<FragmentOnboardingIntroBinding>(
     R.layout.fragment_onboarding_intro
 ) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.actionOnBoardingIntroFragmentToOnBoardingComfortFragment)
+        }
+    }
 }
