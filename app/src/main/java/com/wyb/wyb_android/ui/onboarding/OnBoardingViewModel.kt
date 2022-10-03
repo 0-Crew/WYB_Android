b155b85d71f2d3c2ff23b42f5d3d9ef43d642882
gaeun23 <0423g@naver.com>
2022-10-03 19:21:55 +0900

    [feat] 온보딩 챌린지뷰 체크박스 기능 구현

---
+++ app/src/main/java/com/wyb/wyb_android/ui/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,37 @@
+package com.wyb.wyb_android.ui.onboarding
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.wyb.wyb_android.util.TripleMediatorLiveData
+
+class OnBoardingViewModel : ViewModel() {
+    val waterCheckBox1 = MutableLiveData(true)
+    val waterCheckBox2 = MutableLiveData(false)
+    val waterCheckBox3 = MutableLiveData(false)
+    val waterCheckBox4 = MutableLiveData(false)
+    val waterCheckBox5 = MutableLiveData(false)
+    val waterCheckBox6 = MutableLiveData(false)
+    val waterCheckBox7 = MutableLiveData(false)
+
+    private val checkedCounter1 =
+        TripleMediatorLiveData(waterCheckBox1, waterCheckBox2, waterCheckBox3)
+    private val checkedCounter2 =
+        TripleMediatorLiveData(waterCheckBox4, waterCheckBox5, waterCheckBox6)
+
+    val levelOfJuice = MediatorLiveData<Int>().apply {
+        addSource(
+            TripleMediatorLiveData(checkedCounter1, checkedCounter2, waterCheckBox7)
+        ) { triple ->
+            val checkedCounterFirst = countChecked(triple.first)
+            val checkedCounterSecond = countChecked(triple.second)
+            var waterCbSeventh = 0
+            if (triple.third == true) waterCbSeventh = 1
+            this.value = checkedCounterFirst + checkedCounterSecond + waterCbSeventh
+        }
+    }
+
+    private fun countChecked(data: Triple<Boolean?, Boolean?, Boolean?>?): Int {
+        return data?.toList()?.filter { it == true }?.size ?: 0
+    }
+}
