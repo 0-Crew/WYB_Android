d777e12b4a2bf7008293230f3b60adfccafc4fbb
gaeun23 <0423g@naver.com>
2022-11-06 00:27:17 +0900

    [feat] webViewClient 추가, web view loading progressBar 추가

--- app/src/main/java/com/wyb/wyb_android/ui/setting/SettingTermsFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/setting/SettingTermsFragment.kt
@@ -1,7 +1,13 @@
 package com.wyb.wyb_android.ui.setting
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.wyb.wyb_android.R
@@ -13,20 +19,51 @@ class SettingTermsFragment : BindingFragment<FragmentSettingTermsBinding>(
 ) {
     private val args: SettingTermsFragmentArgs by navArgs()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        initWebView()
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initWebView()
         addListener()
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView() {
         binding.layoutTitle.titleString = getString(args.termsType.title)
-        binding.layoutWebView.loadUrl(getString(args.termsType.url))
+
+        val url = context?.getString(args.termsType.url).orEmpty()
+        binding.layoutWebView.apply {
+            loadUrl(url)
+            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    binding.layoutProgress.visibility = View.VISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.layoutProgress.visibility = View.GONE
+                }
+            }
+        }
     }
 
     private fun addListener() {
         binding.layoutTitle.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+    }
+
+    override fun onDestroyView() {
+        binding.layoutWebView.destroy()
+        super.onDestroyView()
     }
 }
