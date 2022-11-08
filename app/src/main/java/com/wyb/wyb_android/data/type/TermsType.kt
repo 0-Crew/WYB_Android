c7c4abc21e10b7421e9b013bf308653a75572758
gaeun23 <0423g@naver.com>
2022-11-09 03:23:02 +0900

    [refactor] termsType url 주소 변경, 기존 블로그 제거, 미사용 코드 제거

--- app/src/main/java/com/wyb/wyb_android/data/type/TermsType.kt
+++ app/src/main/java/com/wyb/wyb_android/data/type/TermsType.kt
@@ -4,14 +4,14 @@ import com.wyb.wyb_android.R
 
 enum class TermsType(
     val title: Int,
-    val url: Int
+    val url: String
 ) {
     PRIVACY_POLICY(
         title = R.string.setting_privacy_policy,
-        url = R.string.wyb_privacy_policy
+        url = "file:///android_res/raw/wyb_privacy_policy.html"
     ),
     SERVICE_TERMS(
         title = R.string.setting_service_terms,
-        url = R.string.wyb_service_terms
+        url = "file:///android_res/raw/wyb_service_terms.html"
     )
 }
--- app/src/main/java/com/wyb/wyb_android/ui/setting/SettingTermsFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/setting/SettingTermsFragment.kt
@@ -1,13 +1,9 @@
 package com.wyb.wyb_android.ui.setting
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.wyb.wyb_android.R
@@ -34,36 +30,14 @@ class SettingTermsFragment : BindingFragment<FragmentSettingTermsBinding>(
         addListener()
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView() {
         binding.layoutTitle.titleString = getString(args.termsType.title)
-
-        val url = context?.getString(args.termsType.url).orEmpty()
-        binding.layoutWebView.apply {
-            loadUrl(url)
-            settings.javaScriptEnabled = true
-            webViewClient = object : WebViewClient() {
-                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                    super.onPageStarted(view, url, favicon)
-                    binding.layoutProgress.visibility = View.VISIBLE
-                }
-
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    binding.layoutProgress.visibility = View.GONE
-                }
-            }
-        }
+        binding.webView.loadUrl(args.termsType.url)
     }
 
     private fun addListener() {
         binding.layoutTitle.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
-    }
-
-    override fun onDestroyView() {
-        binding.layoutWebView.destroy()
-        super.onDestroyView()
     }
 }
