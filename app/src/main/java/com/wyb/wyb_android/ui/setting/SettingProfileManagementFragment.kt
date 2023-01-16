82d75cc66c8d105bf3a7b55244868a9a462c33e0
gaeun23 <0423g@naver.com>
2023-01-16 17:52:54 +0900

    [feat] 닉네임 수정 완료 시 홈 화면으로 이동, 키보드 action event 설정

--- app/src/main/java/com/wyb/wyb_android/ui/setting/SettingProfileManagementFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/setting/SettingProfileManagementFragment.kt
@@ -2,13 +2,15 @@ package com.wyb.wyb_android.ui.setting
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.wyb.wyb_android.R
 import com.wyb.wyb_android.base.ViewModelFragment
 import com.wyb.wyb_android.databinding.FragmentSettingProfileManagementBinding
 import com.wyb.wyb_android.ui.setting.SettingViewModel.Companion.MAX_NICKNAME_LENGTH
-
+import kotlinx.android.synthetic.main.view_wyb_label_edit_text.view.*
 
 class SettingProfileManagementFragment :
     ViewModelFragment<FragmentSettingProfileManagementBinding, SettingViewModel>(
@@ -26,11 +28,33 @@ class SettingProfileManagementFragment :
         binding.layoutTitle.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        binding.etNickname.ivIcon.setOnClickListener {
+            navigateToHomeFragment()
+        }
+        binding.etNickname.etInput.setOnEditorActionListener(
+            OnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    navigateToHomeFragment()
+                    return@OnEditorActionListener true
+                }
+                false
+            }
+        )
     }
 
     private fun initView() {
         with(binding.etNickname) {
             setTextMaxLength(MAX_NICKNAME_LENGTH)
+            etInput.setImeActionLabel(
+                getString(R.string.complete),
+                EditorInfo.IME_ACTION_DONE
+            )
+        }
+    }
+
+    private fun navigateToHomeFragment() {
+        if(!binding.etNickname.ivIcon.isChecked) {
+            findNavController().navigate(R.id.actionSettingProfileManagementToHome)
         }
     }
 }
