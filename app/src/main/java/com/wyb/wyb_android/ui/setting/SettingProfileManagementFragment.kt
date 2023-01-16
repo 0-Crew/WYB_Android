beb4c25d4109f46d7cb97d95d0670215b8b97124
gaeun23 <0423g@naver.com>
2023-01-16 17:44:57 +0900

    [feat] viewModel 연결, 닉네임 최대 입력글자수 기능 추가

--- app/src/main/java/com/wyb/wyb_android/ui/setting/SettingProfileManagementFragment.kt
+++ app/src/main/java/com/wyb/wyb_android/ui/setting/SettingProfileManagementFragment.kt
@@ -2,22 +2,35 @@ package com.wyb.wyb_android.ui.setting
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.wyb.wyb_android.R
-import com.wyb.wyb_android.base.BindingFragment
+import com.wyb.wyb_android.base.ViewModelFragment
 import com.wyb.wyb_android.databinding.FragmentSettingProfileManagementBinding
+import com.wyb.wyb_android.ui.setting.SettingViewModel.Companion.MAX_NICKNAME_LENGTH
 
-class SettingProfileManagementFragment : BindingFragment<FragmentSettingProfileManagementBinding>(
-    R.layout.fragment_setting_profile_management
-) {
+
+class SettingProfileManagementFragment :
+    ViewModelFragment<FragmentSettingProfileManagementBinding, SettingViewModel>(
+        R.layout.fragment_setting_profile_management
+    ) {
+    override val viewModel: SettingViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
         addListener()
     }
 
     private fun addListener() {
         binding.layoutTitle.btnBack.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun initView() {
+        with(binding.etNickname) {
+            setTextMaxLength(MAX_NICKNAME_LENGTH)
         }
     }
 }
