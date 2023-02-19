1723b4d997748111490e796fb01a9c84e5414403
heewon <ymcho24@naver.com>
2023-02-19 19:23:31 +0900

    [feat] NotificationCelebFragment, NotificationCheerFragment 생성

---
+++ app/src/main/java/com/wyb/wyb_android/ui/notification/NotificationCelebFragment.kt
@@ -0,0 +1,15 @@
+package com.wyb.wyb_android.ui.notification
+
+import android.os.Bundle
+import android.view.View
+import com.wyb.wyb_android.R
+import com.wyb.wyb_android.base.BindingFragment
+import com.wyb.wyb_android.databinding.FragmentNotificationCelebBinding
+
+class NotificationCelebFragment :
+    BindingFragment<FragmentNotificationCelebBinding>(R.layout.fragment_notification_celeb) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/wyb/wyb_android/ui/notification/NotificationCheerFragment.kt
@@ -0,0 +1,15 @@
+package com.wyb.wyb_android.ui.notification
+
+import android.os.Bundle
+import android.view.View
+import com.wyb.wyb_android.R
+import com.wyb.wyb_android.base.BindingFragment
+import com.wyb.wyb_android.databinding.FragmentNotificationCheerBinding
+
+class NotificationCheerFragment :
+    BindingFragment<FragmentNotificationCheerBinding>(R.layout.fragment_notification_cheer) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
