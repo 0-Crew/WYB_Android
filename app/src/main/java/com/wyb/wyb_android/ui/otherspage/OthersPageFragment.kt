ae60f24d8614d2506c1160a53d26e649dfd532c2
gaeun23 <0423g@naver.com>
2023-03-13 21:14:18 +0900

    [feat] 상대방 챌린지 뷰 생성

---
+++ app/src/main/java/com/wyb/wyb_android/ui/otherspage/OthersPageFragment.kt
@@ -0,0 +1,25 @@
+package com.wyb.wyb_android.ui.otherspage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.wyb.wyb_android.R
+import com.wyb.wyb_android.base.ViewModelFragment
+import com.wyb.wyb_android.databinding.FragmentOthersPageBinding
+
+class OthersPageFragment :
+    ViewModelFragment<FragmentOthersPageBinding, OthersPageViewModel>(R.layout.fragment_others_page) {
+    override val viewModel: OthersPageViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.layoutTitle.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
---
+++ app/src/main/java/com/wyb/wyb_android/ui/otherspage/OthersPageViewModel.kt
@@ -0,0 +1,6 @@
+package com.wyb.wyb_android.ui.otherspage
+
+import androidx.lifecycle.ViewModel
+
+class OthersPageViewModel : ViewModel() {
+}
