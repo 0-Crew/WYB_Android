d79bdcbdc56348f18fb355fa9e0bd3503cf4a81f
gaeun23 <0423g@naver.com>
2023-03-30 17:40:42 +0900

    [network] 홈 챌린지 GET api 연동

--- app/src/main/java/com/wyb/wyb_android/network/ChallengeService.kt
+++ app/src/main/java/com/wyb/wyb_android/network/ChallengeService.kt
@@ -1,10 +1,14 @@
 package com.wyb.wyb_android.network
 
+import com.wyb.wyb_android.data.response.HomeResponse
 import com.wyb.wyb_android.data.response.UserHomeResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ChallengeService {
+    @GET("my-challenge/main")
+    suspend fun getHome(): HomeResponse
+
     @GET("my-challenge/user")
     suspend fun getUserHome(
         @Query("userId") userId: Int
